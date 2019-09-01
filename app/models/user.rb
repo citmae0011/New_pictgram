d20137fa8fef7a3674c756289d54cfcdbd4563fb
DaikiMaekawa <citmae0229@gmail.com>
2019-09-01 04:10:27 +0000

    second-commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :name, length: { maximum: 15 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { in: 8..32 }
+  
+
+  has_secure_password
+  
+  has_many :topics
+  has_many :favorites
 end
+
